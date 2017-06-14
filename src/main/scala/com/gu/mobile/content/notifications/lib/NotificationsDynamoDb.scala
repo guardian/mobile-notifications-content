919da896e35f4b909406b0167b46ec4328593114
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2017-06-14 11:30:05 +0100

    add expiry timw to content id saved in dynamo

--- src/main/scala/com/gu/mobile/content/notifications/lib/NotificationsDynamoDb.scala
+++ src/main/scala/com/gu/mobile/content/notifications/lib/NotificationsDynamoDb.scala
@@ -1,19 +1,21 @@
 package com.gu.mobile.content.notifications.lib
 
-import com.amazonaws.auth.{ AWSCredentialsProviderChain, STSAssumeRoleSessionCredentialsProvider }
+import com.amazonaws.auth.{AWSCredentialsProviderChain, STSAssumeRoleSessionCredentialsProvider}
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec
-import com.amazonaws.services.dynamodbv2.document.{ DynamoDB, Item }
-import com.gu.mobile.content.notifications.{ Config, NotificationsDebugLogger }
+import com.amazonaws.services.dynamodbv2.document.{DynamoDB, Item}
+import com.gu.mobile.content.notifications.{Config, NotificationsDebugLogger}
+import org.joda.time.DateTime
 
 class NotificationsDynamoDb(dynamoDB: DynamoDB, tableName: String) {
 
   val table = dynamoDB.getTable(tableName)
 
   def saveContentItem(contentId: String): Unit = {
-    table.putItem(new Item().withPrimaryKey("contentId", contentId))
+    val expiry = DateTime.now().plusDays(1).getMillis / 1000 //Expiry should be an epoch value: http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/time-to-live-ttl-how-to.html
+    table.putItem(new Item().withPrimaryKey("contentId", contentId).withDouble("expiry", expiry))
   }
 
   def haveSeenContentItem(contentId: String): Boolean = {
