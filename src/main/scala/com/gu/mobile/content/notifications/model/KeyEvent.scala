73655607f9c9f06630db2777173bca44fcc857af
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2017-06-20 10:32:12 +0100

    add date to key events debug

--- src/main/scala/com/gu/mobile/content/notifications/model/KeyEvent.scala
+++ src/main/scala/com/gu/mobile/content/notifications/model/KeyEvent.scala
@@ -1,11 +1,13 @@
 package com.gu.mobile.content.notifications.model
 
-import com.gu.contentapi.client.model.v1.Content
+import com.gu.contentapi.client.model.v1.{CapiDateTime, Content}
+import com.gu.contentapi.client.utils.CapiModelEnrichment.RichCapiDateTime
 
-case class KeyEvent(id: String, title: Option[String], body: String) {
+case class KeyEvent(id: String, title: Option[String], body: String, publishedDate: Option[CapiDateTime]) {
   //TODO - this is just for debugging, blat it later
   lazy val pTitle = title.getOrElse("**")
-  override def toString = s"Key event. Id: ${id} title: ${pTitle}"
+  lazy val pDate = publishedDate.map(_.toString()).getOrElse("**!**")
+  override def toString = s"Key event. Id: ${id} title: ${pTitle}: publised $pDate"
 }
 
 object KeyEvent {
@@ -16,7 +18,7 @@ object KeyEvent {
         .getOrElse(Nil)
         .filter(_.attributes.keyEvent.exists(identity))
         .filter(_.published)
-        .map(block => KeyEvent(block.id, block.title, block.bodyTextSummary))
+        .map(block => KeyEvent(block.id, block.title, block.bodyTextSummary, block.publishedDate))
         .toList
         .reverse
 
