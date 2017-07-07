aa9ec0ada5a17a5755332a37e190d5459057f433
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2017-07-07 17:10:59 +0100

    fix dynamo import

--- src/main/scala/com/gu/mobile/content/notifications/lib/NotificationsDynamoDb.scala
+++ src/main/scala/com/gu/mobile/content/notifications/lib/NotificationsDynamoDb.scala
@@ -1,31 +1,27 @@
 package com.gu.mobile.content.notifications.lib
 
-import com.amazonaws.auth.{ AWSCredentialsProviderChain, STSAssumeRoleSessionCredentialsProvider }
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.auth.{AWSCredentialsProviderChain, STSAssumeRoleSessionCredentialsProvider}
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec
-import com.amazonaws.services.dynamodbv2.document.{ DynamoDB, Item }
-import com.gu.mobile.content.notifications.{ Config, Logging }
-import com.amazonaws.auth.{ AWSCredentialsProviderChain, STSAssumeRoleSessionCredentialsProvider }
-import com.amazonaws.regions.Regions
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
-import com.amazonaws.services.dynamodbv2.document.spec.{ GetItemSpec, PutItemSpec }
-import com.amazonaws.services.dynamodbv2.document.{ DynamoDB, Item }
+import com.amazonaws.services.dynamodbv2.document.{DynamoDB, Item}
+import com.gu.mobile.content.notifications.{Config, Logging}
 import org.joda.time.DateTime
 
-class NotificationsDynamoDb(dynamoDB: DynamoDB, tableName: String) {
+class NotificationsDynamoDb(dynamoDB: DynamoDB, config: Config) {
 
-  val table = dynamoDB.getTable(tableName)
+  val contentTable = dynamoDB.getTable(config.contentDynamoTableName)
+  val liveBlogTable = dynamoDB.getTable(config.liveBlogContentDynamoTableName)
 
   def saveContentItem(contentId: String): Unit = {
     val expiry = DateTime.now().plusDays(1).getMillis / 1000 //Expiry should be an epoch value: http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/time-to-live-ttl-how-to.html
-    table.putItem(new Item().withPrimaryKey("contentId", contentId).withDouble("expiry", expiry))
+    contentTable.putItem(new Item().withPrimaryKey("contentId", contentId).withDouble("expiry", expiry))
   }
 
   def haveSeenContentItem(contentId: String): Boolean = {
     val getItemSpec = new GetItemSpec().withPrimaryKey("contentId", contentId)
-    Option(table.getItem(getItemSpec)).isDefined
+    Option(contentTable.getItem(getItemSpec)).isDefined
     Option(contentTable.getItem(getItemSpec)).isDefined
   }
 
