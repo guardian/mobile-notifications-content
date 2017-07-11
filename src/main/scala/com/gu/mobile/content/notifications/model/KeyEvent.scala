787366fc4860b3014811985f13cb4e1dfceb5ebb
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2017-07-11 14:16:01 +0100

    reverse logic for key events and log

--- src/main/scala/com/gu/mobile/content/notifications/model/KeyEvent.scala
+++ src/main/scala/com/gu/mobile/content/notifications/model/KeyEvent.scala
@@ -1,7 +1,9 @@
 package com.gu.mobile.content.notifications.model
 
-import com.gu.contentapi.client.model.v1.{ CapiDateTime, Content }
+import akka.event.Logging.LoggerException
+import com.gu.contentapi.client.model.v1.{CapiDateTime, Content}
 import com.gu.contentapi.client.utils.CapiModelEnrichment.RichCapiDateTime
+import com.gu.mobile.content.notifications.Logging
 import com.gu.mobile.content.notifications.lib.NotificationsDynamoDb
 
 case class KeyEvent(blockId: String, title: Option[String], body: String, publishedDate: Option[CapiDateTime], lastModified: Option[CapiDateTime]) {
@@ -28,15 +30,19 @@ object KeyEvent {
   }
 }
 
-class KeyEventProvider(notificationsDynamoDb: NotificationsDynamoDb) {
+class KeyEventProvider(notificationsDynamoDb: NotificationsDynamoDb) extends Logging {
 
   def getLatestKeyEvent(content: Content): Option[(Content, KeyEvent)] = {
     KeyEvent.fromContent(content).flatMap { keyEvent =>
+      logger.info(s"Handle content: ${content.id} key event ${keyEvent.blockId}: ")
       notificationsDynamoDb.haveSeenBlogEvent(content.id, keyEvent.blockId) match {
         case true =>
-          notificationsDynamoDb.saveLiveBlogEvent(content.id, keyEvent.blockId)
+          logger.info(s"Already seen key event with block id: ${keyEvent.blockId}")
           None
-        case _ => Some((content, keyEvent))
+        case _ =>
+          logger.info(s"Found new block for content: ${content.id}: with block id ${keyEvent.blockId}")
+          notificationsDynamoDb.saveLiveBlogEvent(content.id, keyEvent.blockId)
+          Some((content, keyEvent))
       }
     }
   }
