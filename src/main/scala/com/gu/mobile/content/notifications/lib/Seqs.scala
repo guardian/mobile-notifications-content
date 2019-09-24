0749bf165c0fad822aa1adac29e6a06b9d16d700
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2019-09-24 11:57:53 +0100

    match on list

--- src/main/scala/com/gu/mobile/content/notifications/lib/Seqs.scala
+++ src/main/scala/com/gu/mobile/content/notifications/lib/Seqs.scala
@@ -3,7 +3,7 @@ package com.gu.mobile.content.notifications.lib
 object Seqs {
 
   implicit class RichSeq[A](as: Seq[A]) {
-    def findOne(f: A => Boolean): Option[A] = as.filter(f) match {
+    def findOne(f: A => Boolean): Option[A] = as.filter(f).toList match {
       case one :: Nil => Some(one)
       case _ => None
     }
